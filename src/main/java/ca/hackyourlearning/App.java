80f87ba51f4ec28a3b99e9dfed4b4d2f7633d79f
Bhavyai Gupta <zbhavyai@gmail.com>
2021-07-10 22:38:41 +0530

    new function created

--- src/main/java/ca/hackyourlearning/App.java
+++ src/main/java/ca/hackyourlearning/App.java
@@ -1,7 +1,13 @@
 package ca.hackyourlearning;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public void start() {
         System.out.println(BitcoinUSD.getUSD());
     }
+
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+        app.start();
+    }
+
 }
