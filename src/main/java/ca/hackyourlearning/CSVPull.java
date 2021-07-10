820b8459698aed075b9341a7df6df3249f98bda5
Bhavyai Gupta <zbhavyai@gmail.com>
2021-07-11 00:34:10 +0530

    adding CSVPull class from Mike

---
+++ src/main/java/ca/hackyourlearning/CSVPull.java
@@ -0,0 +1,46 @@
+package ca.hackyourlearning;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * Reads the CSV file MiningSetup.csv and loads all Miners
+ */
+public class CSVPull {
+    public ArrayList<Miner> minerArray;
+
+    /**
+    * Constructs CSVPull object
+    */
+    public CSVPull() {
+        this.minerArray = new ArrayList<Miner>();
+        this.puller();
+    }
+
+    /**
+     * Load all miners from MiningSetup.csv into minerArray
+     */
+    public void puller() {
+        Scanner reader;
+
+        try {
+            reader = new Scanner(new File("MiningSetup.csv"), "UTF-8");
+            reader.useDelimiter(",");
+
+            while (reader.hasNextLine()) {
+                String[] lines = reader.nextLine().split(",");
+
+                Miner mine = new Miner(lines[0], Double.parseDouble(lines[1]), Double.parseDouble(lines[2]));
+                this.minerArray.add(mine);
+            }
+            reader.close();
+        }
+
+        catch (FileNotFoundException e) {
+            System.err.printf("%n%n[%s] Please make sure \'MiningSetup.csv\' is present at the project directory",
+                    ColorText.text("FAIL", Color.RED));
+        }
+    }
+}
